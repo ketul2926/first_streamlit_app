135e091c5ff644987e33ff5e0f17a0698780e1d8
ketul2926 <133841683+ketul2926@users.noreply.github.com>
2023-05-17 18:15:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
-import pandas 
+import pandas
+import snowflake.connector
 
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favorites')
@@ -33,4 +34,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
+
