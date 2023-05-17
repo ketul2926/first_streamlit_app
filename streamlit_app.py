6668a3a59cfa2d01be4269ca9ad4178c771e6c25
ketul2926 <133841683+ketul2926@users.noreply.github.com>
2023-05-17 10:52:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,8 @@
 import streamlit
-
- 
-
 streamlit.title("My Mom's New Healthy Diner")
-
- 
-
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
- 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
